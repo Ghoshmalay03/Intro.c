bf5ca4e6009842e543691e6f2e78017ab244af59
Malay Ghosh <150132631+Ghoshmalay03@users.noreply.github.com>
2024-03-30 22:19:54 +0530

    Create Intro.c

---
+++ Intro.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+
+    //this code is written to provide some basic information about myself.
+
+    printf("Welcome to my GitHub profile. \n");
+    printf("Hello, myself Malay Ghosh. \n");
+    printf("I am a Btech(CSE) student. \n");
+    return 0;
+}
